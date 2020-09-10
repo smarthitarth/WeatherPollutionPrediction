f0c38fff16daf86568e4b0ad396b73f4a6c17f9f
hitarth <smarthitarth1@gmail.com>
2020-09-10 15:31:49 -0400

    Random Forest Regressor

--- RandomForestRegression.py
+++ RandomForestRegression.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 10 02:29:13 2020
+Created on Thu Sep 10 15:25:36 2020
 
 @author: hitarth
 """
@@ -15,47 +15,29 @@ df = df.dropna()
 
 X = df.iloc[:,:-1]
 y = df.iloc[:, -1]
+
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=7)
 
 #Decision tree
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+rRegressor = RandomForestRegressor()
+rRegressor.fit(X_train, y_train)
 
-dtree = DecisionTreeRegressor(criterion="mse")
-dtree.fit(X_train, y_train)
-
-print("Coeff R^2(train): {}".format(dtree.score(X_train, y_train)))
-print("Coeff R^2(test): {}".format(dtree.score(X_test, y_test)))
+print("Coeff R^2(train): {}".format(rRegressor.score(X_train, y_train)))
+print("Coeff R^2(test): {}".format(rRegressor.score(X_test, y_test)))
 
 #Score
-score = cross_val_score(dtree, X,y,cv=5)
+score = cross_val_score(rRegressor, X,y,cv=5)
 score.mean()
 
-#visualization
-from IPython.display import Image
-from sklearn.externals.six import StringIO
-from sklearn.tree import export_graphviz
-import pydotplus
-
-features = list(df.columns[:-1])
-print(features)
-
-import os 
-os.environ['PATH'] = os.environ['PATH']+';'+os.environ['CONDA_PREFIX']+r"\Library\bin\graphviz"
-
-dot_data = StringIO()
-export_graphviz(dtree, out_file=dot_data, feature_names=features, filled=True, rounded=True)
-
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-
-Image(graph.create_png())
-
-prediction = dtree.predict(X_test)
+prediction = rRegressor.predict(X_test)
 plt.scatter(y_test, prediction)
 
 
-#Hyper parameter optimization
+#Hyper parameter Tuning
+
 params={
         "splitter":["best", "random"],
         "max_depth": [3, 4, 5, 6, 8, 10, 12, 15],
@@ -102,3 +84,6 @@ file = open('DecisionTree_regression.pkl', 'wb')
 
 #dumping info to file
 pickle.dump(random_search, file)
+
+
+
